d48a31a3914e70cefcee59f2f86a2737e74a568d
Austin Wulf <atwulf@gmail.com>
2015-10-18 13:03:22 -0600

    add test for state property isTest

--- tests/specs/stateSpec.js
+++ tests/specs/stateSpec.js
@@ -4,6 +4,16 @@
 
 describe('App#state', function() {
 
+  describe('.isTest', function() {
+    it('should be true', function() {
+      expect(state.isTest).to.be.true;
+    });
 
+    it('should be editable', function() {
+      state.isTest = false;
+      expect(state.isTest).to.be.false;
+      state.isTest = true;
+    });
+  });
 
 });
