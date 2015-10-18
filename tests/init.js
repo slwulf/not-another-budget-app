5853d0ed78e38009044422ce4ca705da68371f5b
Austin Wulf <atwulf@gmail.com>
2015-10-18 09:26:34 -0600

    suppress console.warn in test output

--- tests/init.js
+++ tests/init.js
@@ -5,3 +5,8 @@
 // var assert = require('assert');
 var chai = require('chai');
 var expect = chai.expect;
+
+// Suppress quiet errors from app script
+console.warn = function() {
+  return undefined;
+};
