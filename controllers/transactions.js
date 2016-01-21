18838b2c895eb274ca503c6b5d554162f641c15b
Austin Wulf <atwulf@gmail.com>
2016-01-20 21:38:28 -0700

    add rest api routes and controllers

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,4 +1,5 @@
 var db = require('mongoose');
+var numeral = require('numeral');
 
 /**
  * get
@@ -26,20 +27,86 @@ var get = function get(req, res, next) {
 };
 
 /**
+ * post
+ *
+ * Creates a new transaction given
+ * some data.
+ */
+
+var post = function post(req, res, next) {
+  var description = req.body.description || 'Transaction';
+  var amount = req.body.amount || 0;
+  var category = req.body.category || 'Default';
+  var date = req.body.date || new Date();
+
+  db.model('transactions').create({
+    description: description,
+    amount: amount,
+    category: category,
+    date: date
+  }, function(err) {
+    if (err) next(err);
+    res.redirect('/');
+  });
+};
+
+/**
+ * put
+ *
+ * Updates a specific transaction
+ * given some data and an id.
+ */
+
+var put = function put(req, res, next) {
+  var id = req.body.id;
+  var description = req.body.id;
+  var amount = req.body.amount;
+  var category = req.body.category;
+  var date = req.body.date;
+
+  db.model('transactions').findById(id, function(err, tr) {
+    if (err) next(err);
+
+    if (description) tr.description = description.trim();
+    if (amount) tr.amount = parseFloat(amount);
+    if (category) tr.category = category.trim();
+    if (date) tr.date = new Date(date);
+
+    tr.save(function(err) {
+      if (err) next(err);
+      res.send(tr);
+    });
+  });
+};
+
+/**
+ * remove
+ *
+ * Deletes a specific transaction
+ * given an id.
+ */
+
+var remove = function remove(req, res, next) {
+  db.model('transactions').findByIdAndRemove(req.body.id, function(err) {
+    if (err) next(err);
+  });
+};
+
+/**
  * categories
  *
  * Returns a list of all distinct
  * categories of transactions.
  */
 
-var categories = function categories(req, res, next) {
+var categories = function categories(cb, next) {
   var t = db.model('transactions').find();
   t.distinct('category');
   t.exec(function(err, list) {
     if (err) next(err);
-    res.send(list);
+    cb(list);
   });
-}
+};
 
 /**
  * render
@@ -54,11 +121,14 @@ var render = function render(req, res, next) {
       title: 'Not Another Budget App',
       transactions: list
     });
-  })
+  });
 };
 
 module.exports = {
   get: get,
+  post: post,
+  put: put,
+  remove: remove,
   categories: categories,
   render: render
 };
--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var transactions = require('../../controllers/transactions');
+var importCSV = require('../../config/import');
 
 /**
  * GET
@@ -8,17 +9,35 @@ var transactions = require('../../controllers/transactions');
 
 // static
 router.get('/', transactions.get);
-router.get('/categories', transactions.categories);
+router.get('/categories', function(req, res, next) {
+  transactions.categories(res.send, next);
+});
 
 // variable
 router.get('/:category', transactions.get);
-router.get('/:year/:month', transactions.get);
-router.get('/:year/:month/:category', transactions.get)
+// router.get('/:year/:month', transactions.get);
+// router.get('/:year/:month/:category', transactions.get);
 
 /**
  * POST
  */
 
-// router.post('/import', transactions.importCSV);
+router.post('/new', transactions.post);
+router.post('/import', function(req, res, next) {
+  var csv = req.body.csv;
+  importCSV(csv, res.send, next);
+});
+
+/**
+ * PUT
+ */
+
+router.put('/update', transactions.put);
+
+/**
+ * DELETE
+ */
+
+router.delete('/delete', transactions.remove);
 
 module.exports = router;
