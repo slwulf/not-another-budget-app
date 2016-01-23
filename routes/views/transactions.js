04f2651e07ed91208f6ba45638cdb2cafd3cc2d8
Austin Wulf <atwulf@gmail.com>
2016-01-23 13:56:50 -0700

    add routes for filtering transactions

--- routes/views/transactions.js
+++ routes/views/transactions.js
@@ -3,5 +3,11 @@ var router = express.Router();
 var transactions = require('../../controllers/transactions');
 
 router.get('/', transactions.render);
+router.get('/:category', transactions.render);
+router.get('/:year/:month', transactions.render);
+router.get('/:year/:month/:category', transactions.render);
+router.get('/import', function(req, res, next) {
+  res.render('import');
+});
 
 module.exports = router;
