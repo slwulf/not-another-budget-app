7a5255324ead7175d92bf44ec645544393542741
Austin Wulf <atwulf@gmail.com>
2016-01-23 14:54:15 -0700

    add helper for comparing values

--- config/handlebars.js
+++ config/handlebars.js
@@ -65,6 +65,49 @@ module.exports = hbs.create({
       var day = date.getDate();
       var year = date.getFullYear();
       return month + '/' + day + '/' + year;
+    },
+
+    /**
+     * {{#compare lvalue operator rvalue}}{{/compare}}
+     * Compares lvalue with rvalue given an operator
+     */
+
+    compare: function(lvalue, operator, rvalue, options) {
+      var operators, result;
+
+      if (arguments.length < 3) {
+        throw new Error('Handelbars Helper \'compare\' needs 2 parameters');
+      }
+
+      if (options === undefined) {
+        options = rvalue;
+        rvalue = operator;
+        operator = '===';
+      }
+
+      operators = {
+        '==': function(l, r) { return l == r; },
+        '===': function(l, r) { return l === r; },
+        '!=': function(l, r) { return l != r; },
+        '!==': function(l, r) { return l !== r; },
+        '<': function(l, r) { return l < r; },
+        '>': function(l, r) { return l > r; },
+        '<=': function(l, r) { return l <= r; },
+        '>=': function(l, r) { return l >= r; },
+        'typeof': function(l, r) { return typeof l == r; }
+      };
+
+      if (!operators[operator]) {
+        throw new Error('Handelbars Helper \'compare\' doesn\'t know the operator ' + operator);
+      }
+
+      result = operators[operator](lvalue, rvalue);
+
+      if (result) {
+        return options.fn(this);
+      } else {
+        return options.inverse(this);
+      }
     }
 
   }
