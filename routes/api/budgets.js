368e001c7b0da73f863fd0d61cd13315b37b46f9
Austin Wulf <atwulf@gmail.com>
2017-01-02 11:17:58 -0700

    rewrite budgets controllers using promises

--- routes/api/budgets.js
+++ routes/api/budgets.js
@@ -2,14 +2,50 @@ var express = require('express');
 var router = express.Router();
 var budgets = require('../../controllers/budgets');
 
-router.get('/', budgets.get);
-router.get('/:category', budgets.get);
-router.get('/:category/status', budgets.status);
-
-router.post('/new', budgets.post);
-
-router.put('/update', budgets.put);
-
-router.delete('/delete/:id', budgets.remove);
+router.get('/', get);
+router.get('/:category', get);
+router.post('/new', post);
+router.put('/update', put);
+router.delete('/delete/:id', remove);
 
 module.exports = router;
+
+function get(req, res, next) {
+  budgets.get(req.params.category)
+    .then(function(results) {
+      res.send(results);
+    }).catch(next);
+}
+
+function post(req, res, next) {
+  var name = req.body.name;
+  var amount = req.body.amount;
+
+  budgets.create(name, amount)
+    .then(function() {
+      res.redirect('/budgets');
+    }).catch(next);
+}
+
+function put(req, res, next) {
+  var id = req.body.id;
+  var amount = req.body.amount;
+  var category = req.body.category;
+
+  budgets.edit({id, amount, category})
+    .then(function(budget) {
+      res.send(budget);
+    }).catch(next);
+}
+
+function remove(req, res, next) {
+  var id = req.params.id;
+
+  budgets.remove(id)
+    .then(function() {
+      res.send({
+        status: 200,
+        message: 'Successfully removed budget ' + id
+      });
+    }).catch(next);
+}
