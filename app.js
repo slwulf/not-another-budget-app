5ab9fc19656454de89e49bede2a773563f307219
Austin Wulf <atwulf@gmail.com>
2017-01-08 18:02:42 -0700

    fix scss compiling, remove sourcemaps

--- app.js
+++ app.js
@@ -26,10 +26,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
-  sourceMap: true
+  src: path.join(__dirname, 'public', 'stylesheets'),
+  dest: path.join(__dirname, 'public', 'stylesheets'),
+  outputStyle: 'compressed',
+  sourceMap: false,
+  prefix: '/stylesheets'
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
