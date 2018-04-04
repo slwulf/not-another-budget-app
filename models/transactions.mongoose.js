2b58b998bbe5d66594fd578f64bc5f9af248ff6b
Austin Wulf <atwulf@gmail.com>
2018-04-03 18:30:43 -0600

    move mongoose models to .mongoose files

---
+++ models/transactions.mongoose.js
@@ -0,0 +1,18 @@
+/**
+ * Transactions Model
+ */
+
+// deps
+var db = require('mongoose')
+var Schema = db.Schema
+
+// create schema
+var transactionsSchema = new Schema({
+  description: String,
+  amount: Number,
+  category: String,
+  date: { type: Date, default: Date.now }
+})
+
+// register model with schema
+db.model('transactions', transactionsSchema)
