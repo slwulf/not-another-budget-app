8a15f625c54f3a76afaf54d9cf09a28a8cb6bcf1
Austin Wulf <atwulf@gmail.com>
2018-04-03 18:44:03 -0600

    cleanup sequelize model loading

--- models/index.js
+++ models/index.js
@@ -1,29 +1,25 @@
 const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
-const config = require('../config').database
+const {database} = require('../config')
+const sequelize = new Sequelize(database.name, database.username, database.password, database)
 
-const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL, config)
-  : new Sequelize(config.name, config.username, config.password, config)
-
-const db = {}
-
-fs.readdirSync(__dirname)
-  .filter(file => {
-    return file.indexOf('.') !== 0 &&
-      file !== 'index.js' &&
-      file.indexOf('.mongoose') === -1
+const db = fs.readdirSync(__dirname)
+  .filter(filename => {
+    return filename.indexOf('.js') > 0 &&
+      filename !== 'index.js' &&
+      filename.indexOf('.mongoose') === -1
   })
-  .forEach(file => {
-    const model = sequelize.import(path.join(__dirname, file))
+  .reduce((db, filename) => {
+    const model = sequelize.import(path.join(__dirname, filename))
     const name = model.name.charAt(0).toUpperCase() + model.name.slice(1)
     db[name] = model
-  })
+    return db
+  }, {})
 
-Object.keys(db).forEach(modelName => {
-  if ('associate' in db[modelName]) {
-    db[modelName].associate(db)
+Object.keys(db).forEach(model => {
+  if ('associate' in db[model]) {
+    db[model].associate(db)
   }
 })
 
