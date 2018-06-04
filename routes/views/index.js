e4d071d7e436636e117770a3da59acd00231e545
Austin Wulf <atwulf@gmail.com>
2018-06-04 09:57:44 -0600

    serve built react app from public dir at /react

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const router = express.Router()
 
@@ -6,5 +7,8 @@ const budgets = require('./budgets')
 
 router.use('/', transactions)
 router.use('/budgets', budgets)
+router.get('/react', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html'))
+})
 
 module.exports = router
