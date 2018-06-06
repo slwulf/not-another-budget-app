dda7e23eb418e67fa550b68c258169489768e785
Austin Wulf <atwulf@gmail.com>
2018-06-06 09:27:39 -0600

    add import transactions form

---
+++ src/Components/Forms/ImportTransactions.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+import Form from './Form'
+
+export default class CreateTransaction extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  onSubmit(values) {
+    console.log(values)
+  }
+
+  render(props) {
+    return <React.Fragment>
+      <Form
+        onSubmit={this.onSubmit.bind(this)}
+        submitLabel="Import"
+        fields={[{
+          type: 'textarea',
+          name: 'transactions'
+        }]} />
+    </React.Fragment>
+  }
+}
