9b47cb17c0c150071e563371870d8ded7a8790bf
Austin Wulf <atwulf@gmail.com>
2018-06-06 09:31:01 -0600

    update routes to render each panel separately

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {
 } from 'react-router-dom'
 
 import Header from './Components/Header'
+import Card from './Components/Card'
+import * as Forms from './Components/Forms'
 import './Style/base.scss'
 
 export default function App() {
@@ -18,12 +20,45 @@ export default function App() {
           { to: '/budgets', label: 'Budgets' },
           { to: '/reports', label: 'Reports' }
         ]} />
-        <Switch>
-          <Route exact path="/" render={() => <Redirect to="/transactions" />} />
-          <Route path="/transactions" render={() => <p>transactions!</p>} />
-          <Route path="/budgets" render={() => <p>budgets!</p>} />
-          <Route path="/reports" render={() => <p>reports!</p>} />
-        </Switch>
+        <section className="form-panel">
+          <Switch>
+            <Route exact path="/transactions" render={() => (
+              <Card
+                title="Add a Transaction"
+                children={(
+                  <Forms.CreateTransaction
+                    onSubmit={state => {
+                      console.log('hello from app', state)
+                    }} />
+                )} />
+            )} />
+            <Route exact path="/transactions/import" render={() => (
+              <Card
+                title="Import Transactions"
+                children={(
+                  <Forms.ImportTransactions
+                    onSubmit={state => {
+                      console.log('hello from app', state)
+                    }} />
+                )} />
+            )} />
+            <Route path="/budgets" render={() => <p>add budgets!</p>} />
+          </Switch>
+        </section>
+        <section className="summary-panel">
+          <Switch>
+            <Route path="/transactions" render={() => <p>transactions summary!</p>} />
+            <Route path="/budgets" render={() => <p>budgets summary!</p>} />
+          </Switch>
+        </section>
+        <section className="details-panel">
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/transactions" />} />
+            <Route path="/transactions" render={() => <p>transactions list!</p>} />
+            <Route path="/budgets" render={() => <p>budgets list!</p>} />
+            <Route path="/reports" render={() => <p>reports view!</p>} />
+          </Switch>
+        </section>
       </div>
     </Router>
   )
