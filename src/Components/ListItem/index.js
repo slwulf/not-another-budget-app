4ff00cdaa28ad6cd4e405d26669a6abe93052eab
Austin Wulf <atwulf@gmail.com>
2018-07-10 17:01:23 -0600

    WIP reset

--- src/Components/ListItem/index.js
+++ src/Components/ListItem/index.js
@@ -5,6 +5,13 @@ import './index.scss'
 export default function ListItem(props) {
   return (
     <div className="list-item">
+      {props.onDelete &&
+        <button type="button"
+        className="list-item-delete"
+        onClick={() => props.onDelete()}>
+          ×
+        </button>
+      }
       {props.children}
     </div>
   )
